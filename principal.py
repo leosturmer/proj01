e3b8dc2abc67c145634f6bb6a4508587fac3a691
LEONARDO MACHADO STURMER <03352088004@senacrs.edu.br>
2025-07-07 10:32:09 -0300

    Usando a var autor para msg

---
+++ principal.py
@@ -0,0 +1,6 @@
+# Programa principal
+# Autor: Leonardo
+# 07/07/2025
+
+autor = 'Leonardo'
+
--- programa.py
+++ programa.py
@@ -2,5 +2,8 @@
 # Autor: Leonardo Stürmer
 # 07/07/2025
 
+import principal
+
 print('Programa em Python')
+print(f'{principal.autor}')
 print('------------------')
